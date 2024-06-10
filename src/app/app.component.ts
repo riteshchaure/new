bbfa6620f8766522520dc9456597b5f87f8c3275
bhosaleashok <ashokbhosale9002@gmail.com>
2024-06-10 11:35:46 +0530

    change the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todolist';
+ar:any[]=[]
+  fun(item:any)
+  {
+    this.ar.push({id:this.ar.length,name:item})
+    console.warn(this.ar);
+
+    
+  }
+  
+  remove (a:any)
+  {
+    this.ar=this.ar.filter(i=>i.id!==a)
+  }
 }
